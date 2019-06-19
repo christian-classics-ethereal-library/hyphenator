1da9774c8bbf6b6de66a17efc11e7f2c9bdcd881
Zach DeCook <zdecook@ccel.org>
2019-06-19 15:14:56 -0400

    - wordSyl test: rename function

--- tests/test_wordSyl.py
+++ tests/test_wordSyl.py
@@ -2,7 +2,7 @@
 from hyphenator import wordSyl
 
 
-def test_meterify(capsys):
+def test_wordSyl(capsys):
     wordSyl.main(['', 'tests/amazinggraceV1.flex.txt'])
     captured = capsys.readouterr()
     assert captured.out[0:29] == "amazing\ta-maz-ing\ngrace\tgrace"
