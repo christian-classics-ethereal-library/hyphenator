d6eb7b9ae833e48e188dea6bbd4b5a662c2ae514
Zach DeCook <zdecook@ccel.org>
2019-06-19 12:09:45 -0400

    * Text To Flex: Improve hyphenator fallback

--- hyphenator/textToFlex.py
+++ hyphenator/textToFlex.py
@@ -3,6 +3,7 @@ import fileinput
 import sys
 import yaml
 
+from nltk.corpus import cmudict
 from nltk.tokenize import SyllableTokenizer
 
 
@@ -26,12 +27,41 @@ class MultiSylT(object):
         self.SSP = SyllableTokenizer()
         with open('dict.yaml') as f:
             self.dict = yaml.safe_load(f)
+        self.d = cmudict.dict()
 
     def multiTokenize(self, word):
         """ Return options for tokenizing a word. """
+        tokenizations = []
+        # If the word exists in our dictionary, return those tokenizations.
         if(word in self.dict['words']):
-            return self.dict['words'][word]
-        return [self.SSP.tokenize(word)]
+            tokenizations += self.dict['words'][word]
+
+        # Otherwise, use an algorithm to get word split up into syllables
+        tokenized = self.SSP.tokenize(word)
+        numberOfSyllables = self.nsyl(word)
+
+        # If the tokenized version has the same number of syllables as
+        # one of the CMU STT pronunciations, return that.
+        if(len(tokenized) in numberOfSyllables and tokenized not in tokenizations):
+            tokenizations.append(tokenized)
+        if(1 in numberOfSyllables and [word] not in tokenizations):
+            tokenizations.append([word])
+
+        # Fallback if there are no tokenizations.
+        if(len(tokenizations) == 0):
+            sys.stderr.write(f'{tokenized} has {len(tokenized)} syllables, '
+                             + 'expected:'
+                             + " or ".join(map(str, numberOfSyllables)) or "unknown")
+            tokenizations.append(tokenized)
+        return tokenizations
+
+    def nsyl(self, word):
+        """ Scan the CMU pronunciation dictionary to get the number of syllables
+        a word should be. """
+        if word in self.d:
+            return [len(list(y for y in x if y[-1].isdigit()))
+                    for x in self.d[word.lower()]]
+        return []
 
 
 if __name__ == "__main__":  # pragma: no cover
