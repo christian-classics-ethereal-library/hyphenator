0e4e19f0121fd6703868bacfed51b8fa87a35298
Zach DeCook <zdecook@ccel.org>
2019-07-18 14:15:59 -0400

    * Hyphenator: Make this a package

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def moduleslist(directory):
+    """Return a list with names for all modules under the given directory."""
+    folder, basename = os.path.split(directory)
+    theList = []
+    for file in os.listdir(directory):
+        if(file[-3:] == '.py' and file[0] != '_'):
+            theList.append(basename + '.' + file[0:-3])
+    return theList
+
+
+setup(
+    name="Hyphenator",
+    version="0.1.0",
+    description="Split words into syllables based on meter, etc.",
+    maintainer="zdecook",
+    maintainer_email="zdecook@ccel.org",
+    url="https://gitlab.ccel.org/zdecook/hymn2yaml",
+    package_list=['hyphenator'],
+    py_modules=moduleslist('./hyphenator'),
+    install_requires=[
+        'nltk',
+        'PyYAML',
+    ],
+)
