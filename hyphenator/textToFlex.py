cbb76fb5b8b7fb54e58d817dd2a4d4fda86efee1
Zach DeCook <zdecook@ccel.org>
2019-08-20 14:58:44 -0400

    - Deformat: fix line length for replace smart single quotes

--- hyphenator/textToFlex.py
+++ hyphenator/textToFlex.py
@@ -147,7 +147,12 @@ class MultiSylT(object):
                     originalWord for x in range(0, len(tokenizations))]))
 
     def deformat(self, word):
-        return word.lower().strip(wordSyl.puncs).replace("\u2019", "'").replace("\u2018", "'")
+        return word.lower().strip(
+            wordSyl.puncs).replace(
+            "\u2019",
+            "'").replace(
+            "\u2018",
+            "'")
 
     def reformat(self, oldTokenized, template):
         # Since tokenized is mutable, create a duplicate of it.
